16355307630308e1ce9bbd87ea5741514c136617
Marc-olivier Gagner <mo@noxe.ca>
2023-01-29 14:03:46 -0500

    T1 completed

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer(props) {
+  return (
+    <footer>
+        <p>Coordonnées : Marc-Olivier Gagner, marc-olivier.gagner.1@ens.etsmtl.ca, Equipe 01-01.</p><br></br>
+        <p>Coordonnées : [Nom], [Email], [Numéro d'équipe].</p><br></br>
+        <p>Coordonnées : [Nom], [Email], [Numéro d'équipe].</p><br></br>
+        <p>Coordonnées : [Nom], [Email], [Numéro d'équipe].</p><br></br>
+  </footer>
+  )
+}
+
+export default Footer
